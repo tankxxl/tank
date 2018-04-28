906883e09f04c96ee91237a134d124ecb4e3ed93
任刚在 <tank2xxl@gmail.com>
2018-04-28 17:31:07 +0800

    add lib jxls-reader

--- src/main/java/com/thinkgem/jeesite/modules/project/web/contract/ProjectContractController.java
+++ src/main/java/com/thinkgem/jeesite/modules/project/web/contract/ProjectContractController.java
@@ -5,10 +5,13 @@ package com.thinkgem.jeesite.modules.project.web.contract;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.thinkgem.jeesite.common.beanvalidator.BeanValidators;
 import com.thinkgem.jeesite.common.config.Global;
 import com.thinkgem.jeesite.common.persistence.Page;
 import com.thinkgem.jeesite.common.service.BaseService;
 import com.thinkgem.jeesite.common.utils.StringUtils;
+import com.thinkgem.jeesite.common.utils.excel.ExportExcel;
+import com.thinkgem.jeesite.common.utils.excel.ImportExcel;
 import com.thinkgem.jeesite.common.web.BaseController;
 import com.thinkgem.jeesite.modules.act.entity.Act;
 import com.thinkgem.jeesite.modules.act.service.ActTaskService;
@@ -17,6 +20,8 @@ import com.thinkgem.jeesite.modules.project.entity.bidding.ProjectBidding;
 import com.thinkgem.jeesite.modules.project.entity.contract.ProjectContract;
 import com.thinkgem.jeesite.modules.project.entity.contract.ProjectContractItem;
 import com.thinkgem.jeesite.modules.project.service.contract.ProjectContractService;
+import com.thinkgem.jeesite.modules.sys.entity.User;
+import com.thinkgem.jeesite.modules.sys.service.SystemService;
 import com.thinkgem.jeesite.modules.sys.utils.ExportUtils;
 import com.thinkgem.jeesite.modules.sys.utils.POIUtils;
 import com.thinkgem.jeesite.modules.sys.utils.UserUtils;
@@ -26,14 +31,13 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.ConstraintViolationException;
 import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.util.Iterator;
@@ -302,5 +306,97 @@ public class ProjectContractController extends BaseController {
 		return;
 
 	}
+
+	/**
+	 * 导入合同数据
+	 * @param file
+	 * @param redirectAttributes
+	 * @return
+	 */
+	@RequiresPermissions("project:contract:projectContract:edit")
+	@RequestMapping(value = "import", method=RequestMethod.POST)
+	public String importFile(MultipartFile file, RedirectAttributes redirectAttributes) {
+		try {
+			System.out.println();
+			int successNum = 0;
+			int failureNum = 0;
+			StringBuilder failureMsg = new StringBuilder();
+			ImportExcel ei = new ImportExcel(file, 1, 0);
+			List<User> list = ei.getDataList(User.class);
+			for (User user : list){
+				try{
+					// if ("true".equals(checkLoginName("", user.getLoginName()))){
+					// 	user.setPassword(SystemService.entryptPassword("123456"));
+					// 	BeanValidators.validateWithException(validator, user);
+					// 	systemService.saveUser(user);
+					// 	successNum++;
+					// }else{
+					// 	failureMsg.append("<br/>登录名 "+user.getLoginName()+" 已存在; ");
+					// 	failureNum++;
+					// }
+				}catch(ConstraintViolationException ex){
+					failureMsg.append("<br/>登录名 "+user.getLoginName()+" 导入失败：");
+					List<String> messageList = BeanValidators.extractPropertyAndMessageAsList(ex, ": ");
+					for (String message : messageList){
+						failureMsg.append(message+"; ");
+						failureNum++;
+					}
+				}catch (Exception ex) {
+					failureMsg.append("<br/>登录名 "+user.getLoginName()+" 导入失败："+ex.getMessage());
+				}
+			}
+			if (failureNum>0){
+				failureMsg.insert(0, "，失败 "+failureNum+" 条用户，导入信息如下：");
+			}
+			addMessage(redirectAttributes, "已成功导入 "+successNum+" 条用户"+failureMsg);
+		} catch (Exception e) {
+			addMessage(redirectAttributes, "导入用户失败！失败信息："+e.getMessage());
+		}
+		return "redirect:" + adminPath + "/project/contract/projectContract/list?repage";
+	}
+
+	/**
+	 * 下载导入合同数据模板
+	 * @param response
+	 * @param redirectAttributes
+	 * @return
+	 */
+	@RequiresPermissions("project:contract:projectContract:edit")
+	@RequestMapping(value = "import/template")
+	public String importFileTemplate(HttpServletResponse response, RedirectAttributes redirectAttributes) {
+		try {
+			String fileName = "合同导入模板.xlsx";
+			List<User> list = Lists.newArrayList();
+			list.add(UserUtils.getUser());
+			new ExportExcel("用户数据", User.class, 2)
+					.setDataList(list)
+					.write(response, fileName)
+					.dispose();
+			return null;
+		} catch (Exception e) {
+			addMessage(redirectAttributes, "导入模板下载失败！失败信息："+e.getMessage());
+		}
+		return "redirect:" + adminPath + "/sys/user/list?repage";
+	}
+
+	/**
+	 * 验证合同编号是否有效，不能重复
+	 * @param oldContractCode
+	 * @param contractCode
+	 * @return
+	 */
+	@ResponseBody
+	@RequiresPermissions("project:contract:projectContract:edit")
+	@RequestMapping(value = "checkContractCode")
+	public String checkContractCode(String oldContractCode, String contractCode) {
+		if (contractCode !=null && contractCode.equals(oldContractCode)) {
+			return "true";
+		}
+		// else if (contractCode !=null && systemService.getUserByLoginName(contractCode) == null) {
+		// 	return "true";
+		// }
+		return "false";
+	}
+
 
 }
--- src/main/webapp/WEB-INF/views/modules/project/contract/projectContractList.jsp
+++ src/main/webapp/WEB-INF/views/modules/project/contract/projectContractList.jsp
@@ -28,6 +28,11 @@
 					}
 				});
 			});
+
+            $("#btnImport").click(function(){
+                $.jBox($("#importBox").html(), {title:"导入数据", buttons:{"关闭":true},
+                    bottomText:"导入文件不能超过5M，仅允许导入“xls”或“xlsx”格式文件！"});
+            });
 		});
 		
 		function page(n,s){
@@ -39,6 +44,14 @@
 	</script>
 </head>
 <body>
+<div id="importBox" class="hide">
+	<form id="importForm" action="${ctx}/project/contract/projectContract/import" method="post" enctype="multipart/form-data"
+		  class="form-search" style="padding-left:20px;text-align:center;" onsubmit="loading('正在导入，请稍等...');"><br/>
+		<input id="uploadFile" name="file" type="file" style="width:330px"/><br/><br/>　　
+		<input id="btnImportSubmit" class="btn btn-primary" type="submit" value="   导    入   "/>
+		<a href="${ctx}/project/contract/projectContract/import/template">下载模板</a>
+	</form>
+</div>
 <ul class="nav nav-tabs">
 	<li class="active"><a href="${ctx}/project/contract/projectContract/">合同列表</a></li>
 	<shiro:hasPermission name="project:contract:projectContract:edit"><li><a href="${ctx}/project/contract/projectContract/form">合同添加</a></li></shiro:hasPermission>
