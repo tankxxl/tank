41be62fd21bfcb3230638c322f5a6d118c88ce5f
任刚在 <tank2xxl@gmail.com>
2019-01-11 17:32:54 +0800

    bootstrap table comments

--- src/main/java/com/thinkgem/jeesite/modules/api/CustomerRestController.java
+++ src/main/java/com/thinkgem/jeesite/modules/api/CustomerRestController.java
@@ -12,8 +12,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -113,5 +117,48 @@ public class CustomerRestController {
     }
 
     // Delete a Client
+
+    // 上传图片
+
+    /**
+     * springmvc.xml:
+     * <!-- 文件上传,id必须设置为multipartResolver -->
+     *     <bean id="multipartResolver"
+     *           class="org.springframework.web.multipart.commons.CommonsMultipartResolver">
+     *         <!-- 设置文件上传大小 -->
+     *         <property name="maxUploadSize" value="5000000" />
+     *     </bean>
+     *
+     * jsp页面：
+     * <form action="/product/upload" method="post" enctype="multipart/form-data">
+     *     <input type="file" name="imgFile">
+     *     <button type="submit">提交</button>
+     * </form>
+     * @param imgFile
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping("/upload")
+    public String upload(MultipartFile imgFile, HttpServletRequest request) throws IOException {
+        // 获取文件名
+        String fileName = imgFile.getOriginalFilename();
+        // 获取图片后缀
+        String extName = fileName.substring(fileName.lastIndexOf("."));
+        if(extName.equals(".jpg")){
+            // 项目在容器中实际发布运行的根路径
+            String realPath=request.getSession().getServletContext().getRealPath("/");
+
+            // 自定义的文件名称
+            String trueFileName = System.currentTimeMillis() + fileName;
+            // 设置存放图片文件的路径
+            String path=realPath+"/upload/" +trueFileName;
+            // 开始上传
+            imgFile.transferTo(new File(path));
+        }
+
+        return "list";
+    }
+
 
 }
